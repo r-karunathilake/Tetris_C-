fab5ec0253b307388a7c76c5745d7487452f745b
Ravindu Karunathilake <karunath@ualberta.ca>
2024-02-04 16:34:21 -0700

    Added functionality to rotate and horizontally move the Tetris tiles.

--- tetris.cpp
+++ tetris.cpp
@@ -6,20 +6,21 @@
 /* TETRIS CONSTRUCTOR */
 /**********************/
 Tetris::Tetris(){
-  window = std::make_shared<sf::RenderWindow>(
+  
+  m_window = std::make_shared<sf::RenderWindow>(
     sf::VideoMode(360, 720), 
     "Tetris C++", 
     sf::Style::Titlebar | sf::Style::Close);
 
-  window->setPosition(sf::Vector2i(100, 100));
+  m_window->setPosition(sf::Vector2i(100, 100));
   
-  if(!tiles.loadFromFile("./assets/squares.png")){
+  if(!m_tiles.loadFromFile("./assets/squares.png")){
     std::cerr << "Error: unable to load tile assets:w" << std::endl; 
   }
 
-  sprite = std::make_shared<sf::Sprite>();
-  sprite->setTexture(tiles);
-  sprite->setScale(sf::Vector2f(0.1, 0.1));
+  m_sprite = std::make_shared<sf::Sprite>();
+  m_sprite->setTexture(m_tiles);
+  m_sprite->setScale(sf::Vector2f(0.1, 0.1));
 }
 
 /***********************/
@@ -29,11 +30,23 @@ void Tetris::events(){
   auto event {std::make_shared<sf::Event>()};
   
   // Check pending events 
-  while(window->pollEvent(*event)){
+  while(m_window->pollEvent(*event)){
     switch(event->type){   
       // Close window
       case sf::Event::Closed:{
-        window->close(); 
+        m_window->close(); 
+      }break;
+
+      case sf::Event::KeyPressed:{
+        if(event->key.code == sf::Keyboard::Up){
+          rotate = true;
+        }
+        else if(event->key.code == sf::Keyboard::Left){
+          --dirx;
+        }
+        else if(event->key.code == sf::Keyboard::Right){
+          ++dirx;
+        }
       }break;
     }
   }
@@ -41,21 +54,64 @@ void Tetris::events(){
 
 void Tetris::draw(){
   // Clear the screen with black color
-  window->clear(sf::Color::Black);
-  window->draw(*sprite); 
+  m_window->clear(sf::Color::Black);
+
+  for(std::size_t idx {0}; idx < s_numSquares; ++idx){
+    m_sprite->setPosition(m_z[idx].x * 36, m_z[idx].y * 36);
+    m_window->draw(*m_sprite); 
+  }
+
   // End drawing current frame and display
-  window->display();
+  m_window->display();
+}
+
+void Tetris::moveDown(){
+  std::uint32_t number {3};
+  
+  if(m_z[0].x == 0){
+    for(std::size_t idx {0}; idx < s_numSquares; ++idx){
+      m_z[idx].x = m_shapes[number][idx] % 2;
+      m_z[idx].y = m_shapes[number][idx] / 2;
+    }
+  }
+}
+
+void Tetris::changePosition(){
+  for(std::size_t idx {0}; idx < s_numSquares; ++idx){
+    m_z[idx].x += dirx;
+  }
+}
+
+void Tetris::setRotate(){
+  if(rotate){
+    Coordinate cord = m_z[1];
+    for(std::size_t idx{0}; idx < s_numSquares; ++idx){
+      int x = m_z[idx].y - cord.y;
+      int y = m_z[idx].x - cord.x;
+
+      m_z[idx].x = cord.x - x;
+      m_z[idx].y = cord.y + y;
+    }
+  }
+}
+
+void Tetris::resetValues(){
+  dirx = 0;
+  rotate = false;
 }
 
 /***********************/
 /*    PUBLIC METHODS   */
 /***********************/
 void Tetris::run(){
-  while(window->isOpen()){
+  while(m_window->isOpen()){
     events();
+    
+    changePosition();
+    setRotate();
+    moveDown();
+    resetValues(); 
+    
     draw(); 
   }
 }
-
-
-
--- tetris.h
+++ tetris.h
@@ -5,15 +5,49 @@
 
 class Tetris{
   private:
-    std::shared_ptr<sf::RenderWindow> window {}; 
-    std::shared_ptr<sf::Sprite> sprite {}; 
-    sf::Texture tiles {}; 
+    static constexpr std::size_t s_numLines  {20};
+    static constexpr std::size_t s_numCols   {10};
+    static constexpr std::size_t s_numSquares {4};
+    static constexpr std::size_t s_numShapes  {7};
+
+    struct Coordinate{
+      std::uint32_t x {};
+      std::uint32_t y {};
+    };
+
+    // 2D array alias templating 
+    template <typename  T, std::size_t Rows, std::size_t Cols>
+    using array2D = std::array<std::array<T, Cols>, Rows>; 
+    
+    array2D<std::uint32_t, s_numLines, s_numCols> m_grid {};
+    array2D<std::uint32_t, s_numShapes, s_numSquares> m_shapes {{
+          {1, 3, 5, 7}, // I
+          {2, 4, 5, 7}, // Z
+          {3, 5, 4, 6}, // S
+          {3, 5, 4, 7}, // T
+          {2, 3, 5, 7}, // L
+          {3, 5, 7, 6}, // J
+          {2, 3, 4, 5}, // O
+        }};
+
+    
+    std::array<Coordinate, s_numSquares> m_z {}, m_k {};
+    std::shared_ptr<sf::RenderWindow> m_window {}; 
+    std::shared_ptr<sf::Sprite> m_sprite {}; 
+    sf::Texture m_tiles {}; 
+
+    int dirx {0};
+    bool rotate {false};
 
   protected:
     void events();
     void draw();
+    void moveDown(); 
+    void setRotate();
+    void resetValues();
+    void changePosition();
 
   public:
     Tetris();
-    void run(); 
+    void run();
 };
